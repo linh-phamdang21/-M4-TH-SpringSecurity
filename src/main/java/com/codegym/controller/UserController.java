103f6c1a2dcfffb8200f6647c8af9e8dbef366ff
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-06-16 09:58:44 +0700

    Init project

---
+++ src/main/java/com/codegym/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.codegym.controller;
+
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/")
+    public ModelAndView index(){
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
+    }
+
+    @GetMapping("user")
+    public ModelAndView user(Principal principal){
+        System.out.println(principal.getName());
+        ModelAndView modelAndView = new ModelAndView("user");
+        return modelAndView;
+    }
+
+    @GetMapping("admin")
+    public ModelAndView admin(){
+        SecurityContext context = SecurityContextHolder.getContext();
+        System.out.println(context.getAuthentication().getName());
+        ModelAndView modelAndView = new ModelAndView("admin");
+        return modelAndView;
+    }
+}
